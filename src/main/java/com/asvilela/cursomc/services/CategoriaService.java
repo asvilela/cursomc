e9e5118f1f6bca5163b92745b486eee31f1a68c4
Andre Vilela <svilela.andre@gmail.com>
2017-11-18 11:30:47 -0200

    Paginacao com parametros opcicionais na requisicao

--- src/main/java/com/asvilela/cursomc/services/CategoriaService.java
+++ src/main/java/com/asvilela/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.asvilela.cursomc.domain.Categoria;
@@ -50,4 +53,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String oderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), oderBy);
+		return repo.findAll(pageRequest);
+	}
+	
 }
